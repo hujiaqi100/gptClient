fa12fe099131e9ee3d7f648c9cbff12895fb081b
胡家琦 <hujiaqi@seclover.com>
2023-05-05 11:58:24 +0800

    first commit

--- vicky/main.js
+++ vicky/main.js
@@ -10,49 +10,36 @@ process.akPath = process.env.Mode = "development" ? path.join(app.getAppPath(), 
 process.historyFilePath = process.env.Mode = "development" ? path.join(app.getAppPath(), 'history') : path.join(app.getPath('exe'), `../history`)
 process.path = process.env.Mode == 'development' ? app.getAppPath() : app.getPath('exe')
 function createWindow() {
-    // 创建浏览器窗口
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 500,
-        show: false, // 初始不显示窗口
-        frame: false, // 去掉窗口边框
-        backgroundColor: '#222', // 设置背景颜色
+        show: false,
+        frame: false,
+        backgroundColor: '#222',
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false, // 需要使用 node 模块
+            contextIsolation: false
         }
     })
     server(process.path, app)
 
-    // 加载网页
     mainWindow.loadURL("http://localhost:3000")
 
-    // 当窗口关闭时触发
     mainWindow.on('closed', function () {
         mainWindow = null
     })
 
-    // 当窗口完成加载时触发
     mainWindow.webContents.on('did-finish-load', function () {
-        // 给网页发送一条消息
-        // mainWindow.reload()
-        // mainWindow.webContents.send('message', 'hello world')
     })
 
-    // 创建系统托盘图标
     tray = new Tray(path.join(process.path, '../ee.png'))
 
-    // tray = new Tray(path.join(__dirname, 'ee.png'))
-    // 当托盘图标被点击时触发
     tray.on('click', function (event, bounds) {
-        // 获取当前屏幕的宽度和高度
         let { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize
 
-        // 计算窗口的位置
         let x = Math.round(bounds.x + bounds.width / 2 - mainWindow.getSize()[0] / 2)
         let y = Math.round(-height - bounds.y)
 
-        // 如果窗口已经显示，则隐藏窗口，否则显示窗口
         if (mainWindow.isVisible()) {
             mainWindow.hide()
         } else {
@@ -61,24 +48,19 @@ function createWindow() {
         }
     })
 
-    // 当窗口失去焦点时触发
     mainWindow.on('blur', function () {
-        // 隐藏窗口
         mainWindow.hide()
     })
 }
 
-// 当 Electron 完成初始化时触发
 app.on('ready', createWindow)
 
-// 当所有窗口都关闭时触发
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
-// 当 Electron 重新激活时触发
 app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
